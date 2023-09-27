b65c75059635ee299862be1fb2a3d5519bbae005
vinid <chiccobia@gmail.com>
2023-09-27 11:14:14 -0700

    answer generation

--- README.md
+++ README.md
@@ -8,5 +8,48 @@ Training data is available under the `data/training` directory. Where you will f
 
 ## Evaluations
 
+We provide two abstractions in `evals` that can be used to evaluate the responses from various models.
 
+For the HarmfulnessRewardModel.
 
+```python
+user_texts = [
+    "User Request 1",
+    "User Request 2",
+]
+assistant_texts = [
+    "Assistant Response 1",
+    "Assistant Response 2",
+]
+
+# Build the conversation with the correct template.
+conversation = ConversationBuilder()
+inputs = [conversation.build(u, a) for u, a in zip(user_texts, assistant_texts)]
+print(inputs)
+
+setup = "redteam"  # or "redteam-osst"
+harmfulness_predictor = AbsoluteHarmfulnessPredictor(setup, device="cuda:0")
+harmfulness_scores = harmfulness_predictor.predict(inputs)
+
+print(harmfulness_scores)
+```
+
+For the OpenAI Evaluator:
+
+```python
+
+cm = ContentModeration()
+scores = cm.content_moderation(assistant_texts)
+
+```
+
+## Generation
+
+```bash
+python generation/generate_answers.py \
+    --prompt_template_path ./configs/alpaca.json \
+    --input_path ${instructions} \
+    --output_path ${output_dir} \
+    --lora_weights ${model} \
+    --load_8bit
+```
---
+++ generation/generate_answers.py
@@ -0,0 +1,253 @@
+import os
+import sys
+import json
+import os.path as osp
+from typing import Union
+
+import torch
+from peft import PeftModel
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+from tqdm import tqdm
+
+from tap import Tap
+
+# Check if GPU is available
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+# Check if MPS is available
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
+
+
+# Arguments class
+class Arguments(Tap):
+    ## Model parameters
+    base_model: str = "yahma/llama-7b-hf"
+    lora_weights: str = "safep/lora-alpaca-small-100-yahma"
+    load_8bit: bool = False
+    auth_token: str = ""
+
+    ## Generation parameters
+    max_new_tokens: int = 256
+    num_beams: int = 4
+    top_k: int = 40
+    top_p: float = 0.75
+    temperature: float = 0.1
+
+    ## Input and output files
+    prompt_template_path: str = "../../configs/alpaca.json"
+    input_path: str = "data/test_input.json"
+    output_path: str = "../output/test_output.json"
+
+
+# Prompter class
+class Prompter(object):
+    __slots__ = ("template", "_verbose")
+
+    def __init__(self, template_name: str = "", verbose: bool = False):
+        self._verbose = verbose
+        if not template_name:
+            # Enforce the default here, so the constructor can be called with '' and will not break.
+            template_name = "alpaca"
+        file_name = template_name  # osp.join("templates", f"{template_name}.json")
+        if not osp.exists(file_name):
+            raise ValueError(f"Can't read {file_name}")
+        with open(file_name) as fp:
+            self.template = json.load(fp)
+        if self._verbose:
+            print(
+                f"Using prompt template {template_name}: {self.template['description']}"
+            )
+
+    def generate_prompt(
+        self,
+        instruction: str,
+        input: Union[None, str] = None,
+        label: Union[None, str] = None,
+    ) -> str:
+        # returns the full prompt from instruction and optional input
+        # if a label (=response, =output) is provided, it's also appended.
+        if input:
+            res = self.template["prompt_input"].format(
+                instruction=instruction, input=input
+            )
+        else:
+            res = self.template["prompt_no_input"].format(instruction=instruction)
+        if label:
+            res = f"{res}{label}"
+        if self._verbose:
+            print(res)
+        return res
+
+    def get_response(self, output: str) -> str:
+        return output.split(self.template["response_split"])[1].strip()
+
+
+# Evaluation function
+def evaluate(
+    model,
+    tokenizer,
+    prompter,
+    instruction,
+    input=None,
+    temperature=0.1,
+    top_p=0.75,
+    top_k=40,
+    num_beams=4,
+    max_new_tokens=128,
+    stream_output=False,
+    **kwargs,
+):
+    prompt = prompter.generate_prompt(instruction, input)
+    inputs = tokenizer(prompt, return_tensors="pt")
+    input_ids = inputs["input_ids"].to(device)
+    generation_config = GenerationConfig(
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
+        num_beams=num_beams,
+        **kwargs,
+    )
+
+    generate_params = {
+        "input_ids": input_ids,
+        "generation_config": generation_config,
+        "return_dict_in_generate": True,
+        "output_scores": True,
+        "max_new_tokens": max_new_tokens,
+    }
+
+    # Without streaming
+    with torch.no_grad():
+        generation_output = model.generate(
+            input_ids=input_ids,
+            generation_config=generation_config,
+            return_dict_in_generate=True,
+            output_scores=True,
+            max_new_tokens=max_new_tokens,
+        )
+    s = generation_output.sequences[0]
+    output = tokenizer.decode(s, skip_special_tokens=True)
+    return prompter.get_response(output)
+
+
+# Main function
+def main(args: Arguments):
+    # Load the input data (.json)
+    input_path = args.input_path
+    with open(input_path) as f:
+        input_data = json.load(f)
+    instructions = input_data["instructions"]
+    inputs = input_data["inputs"]
+
+    # Validate the instructions and inputs
+    if instructions is None:
+        raise ValueError("No instructions provided")
+    if inputs is None or len(inputs) == 0:
+        inputs = [None] * len(instructions)
+    elif len(instructions) != len(inputs):
+        raise ValueError(
+            f"Number of instructions ({len(instructions)}) does not match number of inputs ({len(inputs)})"
+        )
+
+    # Load the prompt template
+    prompter = Prompter(args.prompt_template_path)
+
+    # Load the tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model)
+    if device == "cuda":
+        model = AutoModelForCausalLM.from_pretrained(
+            args.base_model,
+            load_in_8bit=args.load_8bit,
+            torch_dtype=torch.float16,
+            device_map="auto",
+            trust_remote_code=True,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            args.lora_weights,
+            torch_dtype=torch.float16,
+        )
+    elif device == "mps":
+        model = AutoModelForCausalLM.from_pretrained(
+            args.base_model,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+            trust_remote_code=True,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            args.lora_weights,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            args.base_model,
+            device_map={"": device},
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            args.lora_weights,
+            device_map={"": device},
+        )
+
+    if not args.load_8bit:
+        model.half()  # seems to fix bugs for some users.
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+    # Generate the outputs
+    outputs = []
+    for instruction, input in tqdm(
+        zip(instructions, inputs),
+        total=len(instructions),
+        desc=f"Evaluate {args.lora_weights}",
+    ):
+        output = evaluate(
+            model=model,
+            tokenizer=tokenizer,
+            prompter=prompter,
+            instruction=instruction,
+        )
+        outputs.append(output)
+
+    # Save the outputs
+    basename = os.path.basename(input_path)
+
+    output_path = os.path.join(args.output_path, args.lora_weights, basename)
+    # Check if the output path directory exists
+    if not os.path.exists(os.path.dirname(output_path)):
+        os.makedirs(os.path.dirname(output_path))
+    # Save the outputs to the output path
+    with open(output_path, "w") as f:
+        json.dump(
+            {
+                "parameters": {
+                    "model": args.base_model,
+                    "prompt_template": args.prompt_template_path,
+                    "lora_weights": args.lora_weights,
+                    "load_8bit": args.load_8bit,
+                },
+                "inputs": inputs,
+                "instructions": instructions,
+                "outputs": outputs,
+            },
+            f,
+            indent=4,
+        )
+
+
+if __name__ == "__main__":
+    args = Arguments().parse_args()
+    main(args)
